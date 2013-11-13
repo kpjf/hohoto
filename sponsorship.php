d1054cd7009ef76c17f01f1b359660049b747a13
Matt <matt@sayyeah.com>
2013-11-13 12:20:45 -0500

    Updated various css styles, added a sponsorship page/template

---
+++ sponsorship.php
@@ -0,0 +1,117 @@
+<?php
+/*
+Template Name: Sponsorship Page
+*/
+?>
+
+<?php get_header(); ?>
+
+	<div id="sponsorship" class="content">
+		<div class="" id="become_a_sponsor">
+				<h2>Sponsorship</h2>
+				<p>HoHoTO would not be possible without the truly generous support of our many sponsors.</p>
+				<p>A commitment to HoHoTO directly connects your brand with the shiniest and most wonderful members of Toronto’s tech, media, digital marketing, social media and PR communities.</p>
+				<p>If you choose to come on board as a sponsor, it becomes the personal mission of our team to ensure that everyone at the event (and thousands more who are following the live and post-event coverage) will get to know just how terrific you are.</p>
+				<div class="sponsor_container">
+
+
+
+					<h2><img src="<?php echo get_template_directory_uri(); ?>/library/images/icons/icon_sponsor.png"/>Sponsorship Packages</h2>
+					<a href="#" class="button">Become a sponsor</a>
+
+					<ul class="packages">
+						<li>
+							<h2>Candy Cane</h2>
+							<h3>$250 Daily Bread Donation</h3>
+							<ol>
+								<li>2 complimentary tickets to the event</li>
+								<li>2 complimentary drink tickets</li>
+								<li>Your logo on hohoto.ca with a link to your site</li>
+							</ol>
+						</li><li>
+							<h2>Gingerbread</h2>
+							<h3>$500 Daily Bread Donation</h3>
+							<ol>
+								<li>4 complimentary tickets to the event</li>
+								<li>4 complimentary drink tickets</li>
+								<li>Your logo on hohoto.ca with a link to your site</li>
+							</ol>
+						</li><li>
+							<h2>Snowflake</h2>
+							<h3>$1,000 Daily Bread Donation</h3>
+							<ol>
+								<li>6 complimentary tickets to the event</li>
+								<li>6 complimentary drink tickets</li>
+								<li>Your logo on hohoto.ca with a link to your site &amp; displayed at the event</li>
+							</ol>
+						</li><li>
+							<h2>Reindeer</h2>
+							<h3>$2,000 Daily Bread Donation</h3>
+							<ol>
+								<li>8 complimentary tickets to the event</li>
+								<li>8 complimentary drink tickets</li>
+								<li>Your logo on hohoto.ca with a link to your site &amp; displayed at the event</li>
+							</ol>
+						</li><li>
+							<h2>Elf</h2>
+							<h3>$5,000 Daily Bread Donation</h3>
+							<ol>
+								<li>12 complimentary tickets to the event</li>
+								<li>12 complimentary drink tickets</li>
+								<li>Your logo on hohoto.ca with a link to your site &amp; displayed at the event</li>
+							</ol>
+						</li><li>
+							<h2>Mrs. Claus</h2>
+							<h3>$7,500 Daily Bread Donation</h3>
+							<ol>
+								<li>15 complimentary tickets to the event</li>
+								<li>15 complimentary drink tickets</li>
+								<li>Your logo on hohoto.ca with a link to your site &amp; prominently displayed at the event</li>
+							</ol>
+						</li><li>
+							<h2>Santa Claus</h2>
+							<h3>$10,000 Daily Bread Donation</h3>
+							<ol>
+								<li>Access to an exclusive VIP booth</li>
+								<li>18 complimentary tickets to the event</li>
+								<li>1 complimentary bottle of liquor</li>
+								<li>Your logo on hohoto.ca with a link to your site</li>
+								<li>Your logo prominently displayed at the event</li>
+								<li>Other branding and promotional opportunities may be available – please contact us to discuss.</li>
+							</ol>
+							<a href="mailto:sponsors@hohoto.ca?subject=Santa%20Sponsorship" class="button">Get in touch</a>
+						</li><li>
+							<h2>Mrs. Claus</h2>
+							<h3>$7,500 Daily Bread Donation</h3>
+
+							<p>Want to go above and beyond to make this year’s HoHoTO the best one yet? Please contact us to discuss the details of your contribution and our commitment to you, including:</p>
+							<ol>
+								<li>Various branding and promotional opportunities – please contact us to discuss.</li>
+								<li>Access to your own private VIP area</li>
+								<li>20 complimentary tickets to the event</li>
+								<li>2 complimentary bottles of liquor</li>
+								<li>Your logo on hohoto.ca with a link to your site</li>
+								<li>Your logo prominently displayed at the event</li>
+							</ol>
+							<a href="mailto:sponsors@hohoto.ca?subject=Angel%20Sponsorship" class="button">Get in touch</a>
+						</li><li>
+							<h2>In-Kind Sponsorships</h2>
+							<p>If you would like to make a non-cash donation to support the Daily Bread Food Bank, please contact us — we’d love to discuss prizes for our famous raffle on the night of the event, and our “Hangover Auction” in the days following.</p>
+							<a href="mailto:sponsors@hohoto.ca?subject=In-Kind%20Sponsorship" class="button">Get in touch</a>
+						</li><li>
+							<h2>Custom Packages</h2>
+							<p>We love it when sponsors want to get creative! If you have an idea for something unique you’d like to make happen, contact us to figure it out by emailing <a href="mailto:sponsors@hohoto.ca?subject=Custom%20Packages">sponsors@hohoto.ca</a></p>
+
+							<a href="mailto:sponsors@hohoto.ca?subject=Custom%20Packages" class="button">Get in touch</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+				<?php get_sidebar(); ?>
+
+		</div>
+	</div>
+
+			
+
+<?php get_footer(); ?>
